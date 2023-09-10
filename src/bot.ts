a522f255bbb8feec11ef60f0d3e3d84f7fc8b8fc
Askar Arslanov <askarbink29@gmail.com>
2023-09-10 19:23:31 +0300

    fix: validate `gpa` inside `calculateScholarship`

--- src/bot.ts
+++ src/bot.ts
@@ -1,33 +1,36 @@
 import { config } from "./config.ts";
+
 import { Bot } from "grammy";
 
 const bot = new Bot(config.BOT_TOKEN);
 
+/**
+ * @param {number} gpa - TODO
+ * @returns {number} TODO
+ */
 function calculateScholarship(gpa: number): number {
+  if (!(2 <= gpa && gpa <= 5)) {
+    throw new RangeError(/* TODO */);
+  }
+
   const M_MIN = 3000;
   const M_MAX = 20_000;
 
-  const s = M_MIN + (M_MAX - M_MIN) * ((gpa - 2) / 3) ** 2.5;
-  return Math.floor(s / 100) * 100;
+  let s = M_MIN + (M_MAX - M_MIN) * ((gpa - 2) / 3) ** 2.5;
+  s = Math.floor(s / 100) * 100;
+
+  return s;
 }
 
-bot.command("scholarship", async (ctx) => {
-  ctx.reply("Enter your GPA.");
-  //   if (!ctx.message.text) {
-  //     return;
-  //   }
+bot.on("message:text", async (ctx) => {
+  const gpa = parseFloat(ctx.message.text);
 
-  //   const gpa = parseFloat(ctx.message.text);
-
-  //   if (isNaN(gpa)) {
-  //     return;
-  //   }
-
-  //   if (gpa < 2 || gpa > 5) {
-  //     return;
-  //   }
-
-  //   await ctx.reply(calculateScholarship(gpa).toString());
+  try {
+    const scholarship = calculateScholarship(gpa);
+    await ctx.reply(scholarship.toString());
+  } catch (_) {
+    ctx.reply("Invalid input!");
+  }
 });
 
 bot.start();
