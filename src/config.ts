6276cb374d10d4e25280b4e9cc5112822e9f37af
Askar Arslanov <askarbink29@gmail.com>
2023-09-24 18:33:10 +0300

    style: remove extra indentation

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,11 @@
 import { load } from "dotenv";
 import { z } from "zod";
 
-export const Config = z.object({
-  BOT_TOKEN: z.string(),
-});
+export const Config = z.object({ BOT_TOKEN: z.string() });
 export type Config = z.infer<typeof Config>;
 
 async function loadConfig(): Promise<Config> {
-  await load({
-    envPath: ".env",
-    export: true,
-  });
+  await load({ envPath: ".env", export: true });
   return Config.parse(Deno.env.toObject());
 }
 
