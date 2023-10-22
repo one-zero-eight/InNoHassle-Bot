09283a6e84568ca6b9355584e992fa699f833012
Asqar Arslanov <68478201+asqarslanov@users.noreply.github.com>
2023-10-22 13:51:51 +0300

    style(config): envPath

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,10 @@ export const Config = z.object({ BOT_TOKEN: z.string() });
 export type Config = z.infer<typeof Config>;
 
 async function loadConfig(): Promise<Config> {
-  await load({ /* envPath: "./.env", */ export: true });
+  await load({
+    // envPath: "./.env",
+    export: true,
+  });
   return await Config.parseAsync(Deno.env.toObject());
 }
 
