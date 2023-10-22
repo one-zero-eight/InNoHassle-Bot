5fd66430adc482a506d8336ab43005db2ccc7ac4
Asqar Arslanov <68478201+asqarslanov@users.noreply.github.com>
2023-10-22 13:43:16 +0300

    refactor(config): async parse

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,8 @@ export const Config = z.object({ BOT_TOKEN: z.string() });
 export type Config = z.infer<typeof Config>;
 
 async function loadConfig(): Promise<Config> {
-  await load({ envPath: ".env", export: true });
-  return Config.parse(Deno.env.toObject());
+  await load({ /* envPath: "./.env", */ export: true });
+  return await Config.parseAsync(Deno.env.toObject());
 }
 
 export const config = await loadConfig();
