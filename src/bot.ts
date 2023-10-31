3e3f08f98cca8c17b582f69a636c5d148ff59785
Asqar Arslanov <68478201+asqarslanov@users.noreply.github.com>
2023-10-31 12:08:47 +0300

    refactor: converations composer explicit name

--- src/bot.ts
+++ src/bot.ts
@@ -4,13 +4,13 @@ import { MyContext } from "@/bot/context.ts";
 import { i18n } from "@/bot/i18n.ts";
 import { mySession } from "@/bot/session.ts";
 import { config } from "@/config.ts";
-import { composer } from "@/conversations.ts";
+import { composer as conversations } from "@/conversations.ts";
 
 export const bot = new Bot<MyContext>(config.BOT_TOKEN);
 
 bot.use(i18n);
 bot.use(mySession);
-bot.use(composer);
+bot.use(conversations);
 
 bot.catch((e) => console.error(e));
 bot.start();
