02c5a857452c04973572e715d99b52f931f7e920
Asqar Arslanov <68478201+asqarslanov@users.noreply.github.com>
2023-11-09 13:15:59 +0300

    refactor(labels): separate module

---
+++ src/labels/messages.ts
@@ -0,0 +1,14 @@
+/** Messages from `/locales/*.ftl`. */
+export const enum Label {
+  Start = "start",
+  StartHaveAccount = "start-have-account",
+  StartConnectTelegram = "start-connect-telegram",
+  StartCreateAccount = "start-create-account",
+  StartSchedules = "start-schedules",
+  Default = "default",
+  Schedules = "schedules",
+  Settings = "settings",
+  Information = "information",
+  Scholarship = "scholarship",
+  Support = "support",
+}
