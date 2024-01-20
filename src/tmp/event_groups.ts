beaa4edc0b25c08a7fc1f608852ee18de8cb9fdb
Asqar Arslanov <68478201+asqarslanov@users.noreply.github.com>
2024-01-20 18:38:18 +0300

    style!: rename schema keys to camelCase

--- src/tmp/event_groups.ts
+++ src/tmp/event_groups.ts
@@ -2,23 +2,36 @@ import { z } from "zod";
 
 const ZEventGroups = z.object({
   groups: z.array(
-    z.object({
-      id: z.number(),
-      alias: z.string(),
-      updated_at: z.string().transform((str) => new Date(str)),
-      created_at: z.string().transform((str) => new Date(str)),
-      path: z.string(),
-      name: z.string(),
-      description: z.string(),
-      tags: z.array(
-        z.object({
-          id: z.number(),
-          alias: z.string(),
-          type: z.string(),
-          name: z.string(),
-        }),
-      ),
-    }),
+    z
+      .object({
+        id: z.number(),
+        alias: z.string(),
+        updated_at: z.string().transform((str) => new Date(str)),
+        created_at: z.string().transform((str) => new Date(str)),
+        path: z.string(),
+        name: z.string(),
+        description: z.string(),
+        tags: z.array(
+          z.object({
+            id: z.number(),
+            alias: z.string(),
+            type: z.string(),
+            name: z.string(),
+          }),
+        ),
+      })
+      .transform((rename) => {
+        const {
+          updated_at: updatedAt,
+          created_at: createdAt,
+          ...rest
+        } = rename;
+        return {
+          ...rest,
+          updatedAt,
+          createdAt,
+        };
+      }),
   ),
 });
 type EventGroups = z.infer<typeof ZEventGroups>;
